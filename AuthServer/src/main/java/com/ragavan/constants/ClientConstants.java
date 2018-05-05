bc24022f9a577e5aa0c9c15f30747c8434f2a214
RagavanPV <ragav87@ymail.com>
2018-05-05 12:28:20 +0530

    changes for user service based on client

---
+++ AuthServer/src/main/java/com/ragavan/constants/ClientConstants.java
@@ -0,0 +1,11 @@
+package com.ragavan.constants;
+
+public final class ClientConstants {
+
+  private ClientConstants() {
+    // restrict instantiation
+  }
+
+  public static final String ADMIN = "admin";
+  public static final String FRONTEND = "frontend";
+}
--- AuthServer/src/main/java/com/ragavan/service/CustomUserDetailsService.java
+++ AuthServer/src/main/java/com/ragavan/service/CustomUserDetailsService.java
@@ -1,10 +1,13 @@
 package com.ragavan.service;
 
+import com.ragavan.constants.ClientConstants;
 import com.ragavan.data.UserDAO;
 import com.ragavan.model.CustomUserDetail;
 import com.ragavan.model.User;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -17,9 +20,26 @@ public class CustomUserDetailsService implements UserDetailsService {
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    Optional<User> userOptional = userDao.findByUserName(username);
-    if (userOptional.isPresent()) {
-      return userOptional.map(CustomUserDetail::new).get();
+    UsernamePasswordAuthenticationToken authentication =
+        (UsernamePasswordAuthenticationToken)
+            SecurityContextHolder.getContext().getAuthentication();
+    org.springframework.security.core.userdetails.User user =
+        (org.springframework.security.core.userdetails.User) authentication.getPrincipal();
+    String userName = user.getUsername();
+    if (ClientConstants.FRONTEND.equals(user.getUsername())) {
+      Optional<User> userOptional = userDao.findByUserName(username);
+      if (userOptional.isPresent()) {
+        return userOptional.map(CustomUserDetail::new).get();
+      } else {
+        throw new UsernameNotFoundException("User name not found");
+      }
+    } else if (ClientConstants.ADMIN.equals(user.getUsername())) {
+      Optional<User> userOptional = userDao.findByUserName(username);
+      if (userOptional.isPresent()) {
+        return userOptional.map(CustomUserDetail::new).get();
+      } else {
+        throw new UsernameNotFoundException("User name not found");
+      }
     } else {
       throw new UsernameNotFoundException("User name not found");
     }
